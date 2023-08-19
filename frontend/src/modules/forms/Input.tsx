e956b3659906d5663bb98ff24d030eb13cf7b150
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-08-19 01:10:05 -0300

    better readability

--- frontend/src/modules/forms/Input.tsx
+++ frontend/src/modules/forms/Input.tsx
@@ -16,24 +16,23 @@ type InputProps = Omit<
  * Input - A styled input component that handles ARIA attributes for better accessibility.
  * @param props - Contains the usual input properties and additional flags for error and help indicators.
  */
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ hasError = false, hasHelp = false, ...props }, ref) => {
-        const ariaProps = getFormFieldAriaProps({
-            fieldID: props.id,
-            hasError,
-            hasHelp,
-        });
+const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    const { hasError, hasHelp, ...rest } = props;
+    const ariaProps = getFormFieldAriaProps({
+        fieldID: rest.id,
+        hasError: !!hasError,
+        hasHelp: !!hasHelp,
+    });
 
-        return (
-            <input
-                ref={ref}
-                className="border border-gray-200 rounded p-4 block w-full"
-                {...ariaProps}
-                {...props}
-            />
-        );
-    },
-);
+    return (
+        <input
+            ref={ref}
+            className="border border-gray-200 rounded p-4 block w-full"
+            {...ariaProps}
+            {...rest}
+        />
+    );
+});
 
 Input.displayName = 'Input';
 
