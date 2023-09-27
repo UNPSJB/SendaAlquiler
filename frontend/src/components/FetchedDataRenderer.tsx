98b363a278f816a2f4ff313e7bbcfad8eda774d9
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-09-27 17:41:13 -0300

    chore: remove "any" types warning on typescript

---
+++ frontend/src/components/FetchedDataRenderer.tsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type FetchedDataRendererProps<T extends any> = {
+    isLoading: boolean;
+    isError: boolean;
+    data: T;
+    Loading: React.ReactElement;
+    Error: React.ReactElement | null;
+    children: (data: NonNullable<T>) => React.ReactElement;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const FetchedDataRenderer = <T extends any>({
+    isLoading,
+    isError,
+    data,
+    Loading,
+    Error,
+    children,
+}: FetchedDataRendererProps<T>): React.ReactElement | null => {
+    if (isLoading) {
+        return Loading;
+    }
+
+    if (isError) {
+        return Error;
+    }
+
+    if (data) {
+        return children(data);
+    }
+
+    return null;
+};
+
+export default FetchedDataRenderer;
