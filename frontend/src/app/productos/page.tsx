bcc0ea1420c4b7ff74ad69fc50b9680c1d464e33
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-10-20 14:47:37 -0300

    add buttons on list pages headers for create items

--- frontend/src/app/productos/page.tsx
+++ frontend/src/app/productos/page.tsx
@@ -2,10 +2,18 @@ import DashboardLayout, {
     DashboardLayoutBigTitle,
 } from '@/modules/dashboard/DashboardLayout';
 
+import Button from '@/components/Button';
+
 const Page = () => {
     return (
         <DashboardLayout
-            header={<DashboardLayoutBigTitle>Productos</DashboardLayoutBigTitle>}
+            header={
+                <div className="flex items-center justify-between">
+                    <DashboardLayoutBigTitle>Productos</DashboardLayoutBigTitle>
+
+                    <Button href="/productos/add">+ Añadir producto</Button>
+                </div>
+            }
         />
     );
 };
