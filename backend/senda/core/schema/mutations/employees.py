1a01dc06c60fd5e7b44aca73b6cfd085ebbbb2c5
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-11-10 16:42:45 -0300

    split

--- backend/senda/core/schema/mutations/employees.py
+++ backend/senda/core/schema/mutations/employees.py
@@ -5,21 +5,24 @@ from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 from senda.core.models.employees import EmployeeModel
 from users.models import UserModel
-from senda.core.schema.types import Employee
-from utils.graphene import input_object_type_to_dict
+from senda.core.schema.custom_types import Employee
+
 
 class ErrorMessages:
     USER_NOT_FOUND = "El empleado no existe"
+
 
 class CreateEmployeeInput(graphene.InputObjectType):
     first_name = graphene.String(required=True)
     last_name = graphene.String(required=True)
     email = graphene.String(required=True)
-    password = graphene.String(required=True) 
+    password = graphene.String(required=True)
+
 
 class UpdateEmployeeInput(graphene.InputObjectType):
     id = graphene.ID(required=True)
     user_id = graphene.ID()
+
 
 def get_user(user_id: str):
     if user_id:
@@ -27,6 +30,9 @@ def get_user(user_id: str):
             return UserModel.objects.get(id=user_id)
         except ObjectDoesNotExist:
             raise ValueError(ErrorMessages.USER_NOT_FOUND)
+    else:
+        return None
+
 
 class CreateEmployee(graphene.Mutation):
     employee = graphene.Field(Employee)
@@ -37,17 +43,18 @@ class CreateEmployee(graphene.Mutation):
 
     def mutate(self, info: Any, employee_data: CreateEmployeeInput):
         try:
-            employee = EmployeeModel.objects.create_employee(
-                user=UserModel.objects.create(
-                    first_name=employee_data.first_name,
-                    last_name=employee_data.last_name,
-                    email=employee_data.email,
-                    password=employee_data.password
-                )
+            user = UserModel.objects.create(
+                first_name=employee_data.first_name,
+                last_name=employee_data.last_name,
+                email=employee_data.email,
+                password=employee_data.password,
             )
+
+            employee = EmployeeModel.objects.create_employee(user=user)
             return CreateEmployee(employee=employee)
         except Exception as e:
             return CreateEmployee(error=e)
+
 
 class Mutation(graphene.ObjectType):
     create_employee = CreateEmployee.Field()
