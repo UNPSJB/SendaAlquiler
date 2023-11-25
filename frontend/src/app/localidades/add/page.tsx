67b009df54d18e7dea670a733be9d989bd8f9126
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-11-25 16:13:57 -0300

    fix: redirect to localities list after succesful creation

--- frontend/src/app/localidades/add/page.tsx
+++ frontend/src/app/localidades/add/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import CreateLocalityForm from '@/modules/create-forms/CreateLocalityForm';
 
 const Page = () => {
-    return <CreateLocalityForm cancelHref="/localidades" />;
+    const router = useRouter();
+
+    return (
+        <CreateLocalityForm
+            cancelHref="/localidades"
+            onSuccess={() => {
+                router.push('/localidades');
+            }}
+        />
+    );
 };
 
 export default Page;
