99bf737daa0d0053afb83c52550d7e0b55938c3e
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-12-04 03:18:03 -0300

    fix: remove unused setcookie muttation

--- frontend/src/api/hooks/auth.ts
+++ frontend/src/api/hooks/auth.ts
@@ -1,13 +1,7 @@
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
 
 import { fetchClient } from '../fetch-client';
-import {
-    LoginDocument,
-    LoginMutation,
-    LoginMutationVariables,
-    SetSessionOfficeCookieDocument,
-    SetSessionOfficeCookieMutation,
-} from '../graphql';
+import { LoginDocument, LoginMutation, LoginMutationVariables } from '../graphql';
 
 /**
  * Type definition for the options when using the login hook.
@@ -23,22 +17,5 @@ type UseLoginOptions = UseMutationOptions<LoginMutation, Error, LoginMutationVar
 export const useLogin = (options: UseLoginOptions = {}) => {
     return useMutation<LoginMutation, Error, LoginMutationVariables>((data) => {
         return fetchClient(LoginDocument, data);
-    }, options);
-};
-
-type UseSetSessionOfficeCookieOptions = UseMutationOptions<
-    SetSessionOfficeCookieMutation,
-    Error,
-    string
->;
-
-export const useSetSessionOfficeCookie = (
-    options: UseSetSessionOfficeCookieOptions = {},
-) => {
-    return useMutation<SetSessionOfficeCookieMutation, Error, string>((id: string) => {
-        return fetchClient(SetSessionOfficeCookieDocument, {
-            officeId: id,
-            clearCookie: false,
-        });
     }, options);
 };
