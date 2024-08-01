9d4c856d53c85de6d0e7d88af00ebaf8cf844d48
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-08-01 15:10:03 -0300

    feat: remove delete client action

--- frontend/src/app/(dashboard)/clientes/page.tsx
+++ frontend/src/app/(dashboard)/clientes/page.tsx
@@ -3,14 +3,11 @@
 import Link from 'next/link';
 
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
-import { MoreVertical } from 'lucide-react';
-import { useState } from 'react';
-import toast from 'react-hot-toast';
 import Skeleton from 'react-loading-skeleton';
 
 import { ClientsQuery } from '@/api/graphql';
 import { useAllLocalities } from '@/api/hooks';
-import { useClients, useDeleteClient } from '@/api/hooks/clients';
+import { useClients } from '@/api/hooks/clients';
 import { useExportClientsCsv } from '@/api/hooks/csv-export';
 
 import DashboardLayout, {
@@ -21,27 +18,9 @@ import { AdminDataTable } from '@/components/admin-data-table';
 import { AdminDataTableLoading } from '@/components/admin-data-table-skeleton';
 import { AdminTableFilter } from '@/components/admin-table-filter';
 import DeprecatedButton, { ButtonVariant } from '@/components/Button';
-import ButtonWithSpinner from '@/components/ButtonWithSpinner';
 import FetchedDataRenderer from '@/components/FetchedDataRenderer';
 import FetchStatusMessageWithButton from '@/components/FetchStatusMessageWithButton';
 import FetchStatusMessageWithDescription from '@/components/FetchStatusMessageWithDescription';
-import { Button } from '@/components/ui/button';
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from '@/components/ui/dialog';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
 
 const columnsHelper = createColumnHelper<Client>();
@@ -102,90 +81,7 @@ const columns: ColumnDef<Client, any>[] = [
             );
         },
     }),
-    columnsHelper.display({
-        id: 'actions',
-        cell: (props) => {
-            const client = props.row.original;
-            return (
-                <div className="flex justify-end">
-                    <RowActions client={client} />
-                </div>
-            );
-        },
-    }),
 ] as const;
-
-const RowActions = ({ client }: { client: Client }) => {
-    const deleteMutation = useDeleteClient();
-
-    const [open, setOpen] = useState(false);
-
-    return (
-        <Dialog
-            onOpenChange={(next) => {
-                if (deleteMutation.isPending) {
-                    return;
-                }
-
-                setOpen(next);
-            }}
-            open={open}
-        >
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <MoreVertical className="size-5" />
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent>
-                    <DialogTrigger asChild>
-                        <DropdownMenuItem>Eliminar</DropdownMenuItem>
-                    </DialogTrigger>
-                </DropdownMenuContent>
-            </DropdownMenu>
-
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Confirmar eliminación</DialogTitle>
-                    <DialogDescription>
-                        ¿Estás seguro de que quieres eliminar al cliente &ldquo;
-                        <strong>
-                            <em>{client.email}</em>
-                        </strong>
-                        &rdquo;?
-                    </DialogDescription>
-                </DialogHeader>
-
-                <DialogFooter>
-                    <DialogClose asChild>
-                        <Button variant="secondary">Cancelar</Button>
-                    </DialogClose>
-
-                    <ButtonWithSpinner
-                        onClick={() => {
-                            deleteMutation.mutate(client.id, {
-                                onSuccess: () => {
-                                    toast.success(
-                                        `El cliente ${client.email} ha sido eliminado.`,
-                                    );
-                                    setOpen(false);
-                                },
-                                onError: () => {
-                                    toast.error(
-                                        `No se pudo eliminar el cliente ${client.email}`,
-                                    );
-                                },
-                            });
-                        }}
-                        variant="destructive"
-                        showSpinner={deleteMutation.isPending}
-                    >
-                        Eliminar
-                    </ButtonWithSpinner>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
-};
 
 type Client = ArrayElement<ClientsQuery['clients']['results']>;
 
